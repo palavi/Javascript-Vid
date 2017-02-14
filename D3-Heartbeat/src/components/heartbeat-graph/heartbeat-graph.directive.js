7ba130668fa0fe6ebf6902fbb7c33262bd50d8d1
palavi <sripalavi@gmail.com>
2017-02-14 13:49:13 +0530

    Adding project files
    Base project ready

---
+++ D3-Heartbeat/src/components/heartbeat-graph/heartbeat-graph.directive.js
@@ -0,0 +1,58 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .directive('heartbeatGraph', heartbeatGraph);
+
+    heartbeatGraph.$inject = [];
+
+    function heartbeatGraph() {
+        var directive = {};
+
+        return directive;
+    }
+})();
+
+(function () {
+
+    'use strict';
+
+    angular.module('app.dashboard')
+        .directive('tmplDashboard', directiveFunction)
+        .controller('DashboardController', ControllerFunction);
+
+
+    // ----- directiveFunction -----
+    directiveFunction.$inject = [];
+
+    /* @ngInject */
+    function directiveFunction() {
+
+        var directive = {
+            restrict: 'E',
+            templateUrl: 'components/dashboard/dashboard.html',
+            scope: {
+            },
+            controller: 'DashboardController',
+            controllerAs: 'vm'
+        };
+
+        return directive;
+    }
+
+
+    // ----- ControllerFunction -----
+    ControllerFunction.$inject = ['logger'];
+
+    /* @ngInject */
+    function ControllerFunction(logger) {
+
+        activate();
+
+        function activate() {
+            logger.log('Activated Dashboard View');
+        }
+    }
+
+})();
